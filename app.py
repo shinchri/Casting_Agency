fb9afc37838edb783b5b145389a26eef5f39c478
Chris Shin <chrisshin@Kellys-iMac.hitronhub.home>
2021-07-20 22:49:22 -0500

    created stud endpoint methods

--- app.py
+++ app.py
@@ -5,12 +5,44 @@ from flask_cors import CORS
 from models import setup_db
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-  return app
+    @app.route('/actors', method=["GET"])
+    def get_actors():
+        return "Get Actors"
+    
+    @app.route('/movies', method=["GET"])
+    def get_movies():
+        return "Get Movies"
+
+    @app.route('/actors/<int:actor_id>/delete', method=["DELETE"])
+    def delete_actors(actor_id):
+        return "Delete Actors"
+
+    @app.route('/movies/<int:movie_id>/delete', method=["DELETE"])
+    def delete_movies(movie_id):
+        return "Delete Movies"
+
+    @app.route('/actors/create', method=["POST"])
+    def create_actors():
+        return "Create Actors"
+
+    @app.route('/movies/create', method=["POST"])
+    def create_movies():
+        return "Create movies"
+
+    @app.route('/actors/<int:actor_id>/edit', method=["PATCH"])
+    def edit_actors(actor_id):
+        return "Edit Actors"
+
+    @app.route('/movies/<int:movie_id>/edit', method=["PATCH"])
+    def edit_movies(movie_id):
+        return "Edit Movies"
+
+    return app
 
 app = create_app()
 
