69d1d126b5309f819be7a87159facee7d9fe5a4d
Chris Shin <chrisshin@Kellys-iMac.hitronhub.home>
2021-07-21 18:24:37 -0500

    added error cases

--- app.py
+++ app.py
@@ -42,6 +42,40 @@ def create_app(test_config=None):
     def edit_movies(movie_id):
         return "Edit Movies"
 
+
+    #errors (400, 404, 405, 422)
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": "method not allowed"
+        }), 405
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
     return app
 
 app = create_app()
