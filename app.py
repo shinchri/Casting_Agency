9f7cc3c1b6580085f6f307e0ac8f6a0b899668f0
Chris Shin <chrisshin@Kellys-iMac.hitronhub.home>
2021-07-21 11:45:01 -0500

    fixed port mistake

--- app.py
+++ app.py
@@ -10,35 +10,35 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
-    @app.route('/actors', method=["GET"])
+    @app.route('/actors', methods=["GET"])
     def get_actors():
         return "Get Actors"
     
-    @app.route('/movies', method=["GET"])
+    @app.route('/movies', methods=["GET"])
     def get_movies():
         return "Get Movies"
 
-    @app.route('/actors/<int:actor_id>/delete', method=["DELETE"])
+    @app.route('/actors/<int:actor_id>/delete', methods=["DELETE"])
     def delete_actors(actor_id):
         return "Delete Actors"
 
-    @app.route('/movies/<int:movie_id>/delete', method=["DELETE"])
+    @app.route('/movies/<int:movie_id>/delete', methods=["DELETE"])
     def delete_movies(movie_id):
         return "Delete Movies"
 
-    @app.route('/actors/create', method=["POST"])
+    @app.route('/actors/create', methods=["POST"])
     def create_actors():
         return "Create Actors"
 
-    @app.route('/movies/create', method=["POST"])
+    @app.route('/movies/create', methods=["POST"])
     def create_movies():
         return "Create movies"
 
-    @app.route('/actors/<int:actor_id>/edit', method=["PATCH"])
+    @app.route('/actors/<int:actor_id>/edit', methods=["PATCH"])
     def edit_actors(actor_id):
         return "Edit Actors"
 
-    @app.route('/movies/<int:movie_id>/edit', method=["PATCH"])
+    @app.route('/movies/<int:movie_id>/edit', methods=["PATCH"])
     def edit_movies(movie_id):
         return "Edit Movies"
 
@@ -47,4 +47,4 @@ def create_app(test_config=None):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-export DATABASE_URL = 'postgres://postgres@localhost:5432/capstone'
-export DATABASE_URL_TEST = 'postgres://postgres@localhost:5432/capstone_test'
+export FLASK_APP=app.py
+export DATABASE_URL='postgres://postgres@localhost:5432/capstone'
+export DATABASE_URL_TEST='postgres://postgres@localhost:5432/capstone_test'
 
 pip3 install -r requirements.txt
